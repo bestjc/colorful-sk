44e24a915f47be77f76012972e5db56b434513bd
john chen <244405035@qq.com>
2015-06-18 14:34:54 +0800

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @title = "Colorful-SK"
+  end
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | #{@title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | #{@title}"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | #{@title}"
   end
 
 end
