545e03a4bb4a12f53251697019eca0c551309222
john chen <244405035@qq.com>
2015-07-06 16:53:36 +0800

    update home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= isotope.pkgd
 //= require fit-columns
-//= require bootstrap
 //= require turbolinks
 //= require_tree .
 $(document).ready( function() {
